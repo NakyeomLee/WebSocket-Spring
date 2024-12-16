89aa634f0285b436696401662418ae2953d7d4a1
lnk <nakyeom0620@gmail.com>
2024-12-16 10:23:16 +0900

    ajax활용

--- src/main/java/com/metacoding/refsocket/chat/ChatController.java
+++ src/main/java/com/metacoding/refsocket/chat/ChatController.java
@@ -1,15 +1,24 @@
 package com.metacoding.refsocket.chat;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @Controller
 public class ChatController {
     private final ChatService chatService;
+
+    @GetMapping("/api")
+    public ResponseEntity<?> api(){
+        List<Chat> models = chatService.findAll();
+        return ResponseEntity.ok(models);
+    }
 
     @GetMapping("/save-form")
     public String saveForm(){
